53c937bb1d018ffefd9899c6ed7b702261be3b7b
Carlos Eduardo Pereira Santana <kaduridu@gmail.com>
2020-11-06 02:16:02 +0000

    Passei do estavel para cá

--- GameOver.js
+++ GameOver.js
@@ -1,21 +1,26 @@
-class GameOver extends Phaser.Scene{
+class GameOver extends Phaser.Scene
+ {
  constructor()
  {
  super('game-over')
  }
 
+
  create() {
   
- const width = this.scale.width
- const height = this.scale.height
+  const width = this.scale.width
+  const height = this.scale.height
 
- this.input.keyboard.once('keydown_SPACE', () => {this.scene.start({
-   preload: preload,
-    create: create,
-    update: update})})
+ //this.input.keyboard.once('keydown_SPACE', () => {this.scene.start('game')})
 
- this.add.text(width * 0.5, height * 0.4, 'Game Over', {fontSize: 48, fontWeight: 'bold'}).setOrigin(0.5)
- this.add.text(width * 0.5, height * 0.58, 'Press space to play again!',{fontSize: 18}).setOrigin(0.5)
- 
+  this.add.image(width*0.5, height*0.5,'gameOver').setScale(0.4);
+
+  const backgroundSound = this.sound.add('musica_fundo') 
+  backgroundSound.setLoop(true);
+  backgroundSound.play()
+  backgroundSound.setVolume(0.050);
+ //this.add.text(width * 0.5, height * 0.58, 'Press space to play again!',{fontSize: 18}).setOrigin(0.5)
+  //this.add.text(width * 0.5, height * 0.63, 'Actual Score: '+ this.scoreRun,{fontSize: 18}).setOrigin(0.5)
+ //this.add.text(width * 0.5, height * 0.68, 'Highscore: '+ this.finalScore,{fontSize: 18}).setOrigin(0.5)
  }
  }
--- fireMission.js
+++ fireMission.js
@@ -1,34 +1,34 @@
-//TODO problema na execução:funções não esperam o término das outras. ver async e wait 
 
 function FireMission(game){
     this.game = game;
+    this.player = game.player;
     this.tree = game.treeFireMission; 
-    this.campfire = game.physics.add.sprite(config.width -200, config.height -120, 'campfire').setImmovable().setInteractive();
+    this.campfire = game.physics.add.sprite(config.width -200, config.height -120, 'campfire','campfire-32.png').setImmovable().setInteractive();
     this.goal = 100;
     this.incrementProgress = 5;
     this.progress = 0;
     this.steps = {
         "collectingMaterial":{"goal":this.goal/2,"msg":"UMMMMMMMMMM....essa árvore parece ótima, vamos começar por aqui, clique sobre ela para coletar os galhos e as folhas mais secas","obj":this.tree},
-        "buildCampfire":{"goal":this.goal,"msg":"clique nos galhos. colocar explicacao",
+        "buildCampfire":{"goal":this.goal,"msg":"Ótimo trabalho, agora precisamos ascender essa fogueira clique nos galhos que você colheu repetidas para fazer fricção e ascender a fogueira. Cuidado para não se queimar! ",
         "obj":this.campfire},
     };
     
     this.campfire.setActive(false).setVisible(false);
-
-    collectingMaterial(this);
+    collectingMaterial(this)
 }
 
 function collectingMaterial(mission){
 
     step = mission.steps["collectingMaterial"];
-    console.log(step["msg"]);
-    
+    alert(step["msg"]);
+    var tree = this.tree;
     step["obj"].on('pointerdown', function() {
         if(mission.progress<step["goal"]){
             mission.progress +=mission.incrementProgress;
-            console.log("Progress: " +mission.progress );
+            //alert("Progress: " +mission.progress );
         }
         if(mission.progress == step["goal"]){
+            tree.setActive(false).setVisible(false);
             buildCampfire(mission);
         }
     });
@@ -36,8 +36,9 @@ function collectingMaterial(mission){
 
 function buildCampfire(mission){
         step = mission.steps["buildCampfire"];
-       
-        this.campfire.setActive(true).setVisible(true);
+        alert(step["msg"]);
+        var campfire = this.campfire;
+        campfire.setActive(true).setVisible(true);
         mission.game.physics.add.collider(mission.campfire, mission.game.player);
 
         //TODO corrigir Movimentos
@@ -45,16 +46,25 @@ function buildCampfire(mission){
         step["obj"].on('pointerdown', function() {
             if(mission.progress<step["goal"]){
                 mission.progress +=mission.incrementProgress;
-                console.log("Progress: " +mission.progress );
+                //alert("Progress: " +mission.progress );
             }
             if(mission.progress == step["goal"]){
-                mission.campfire.setTexture('elements',15);
-                console.log("Sucess");
+                campfire.destroy();
+                mission.game.physics.add.sprite(config.width -200, config.height -120, 'campfire','campfire-19.png').setImmovable().setInteractive();
+                mission.game.scene.start('game-over');
+                //this.add.text(width * 0.5, height * 0.4, 'Game Over', {fontSize: 48, fontWeight: 'bold'}).setOrigin(0.5)
+                //this.scoreLabel = this.add.text(70, 40, 'Score: 0', style).setScrollFactor(0).setOrigin(0.5, 0);
+                //this.scoreLabel.align = 'left';
+                alert("Success");
+                //this.add.text(70,40, 'Success',{fontSize: 24}).setOrigin(0.0).setScrollFactor(0);
+                
             }
         });
-        //exemplo:
-        //const bear = this.physics.add.sprite(256,128,'bear','brown-down-1.png');
 
-        this.campfire.setTexture('campfire','campfire-30.png');
+
+        //exemplo:
+       //this.campfire.setTexture('campfire-19.png');
+
+        //this.campfire.setTexture('campfire','campfire-30.png');
         //this.physics.add.sprite(x,y,'campfire','campfire-20.png');
 }
